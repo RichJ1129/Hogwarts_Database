ac9e3d8eae0010da0fdaab29ea9ad70764dc27c4
Richard Joseph <richjoseph1129@gmail.com>
2019-11-20 22:44:29 -0500

    Server idle error fix

--- dbcon.js
+++ dbcon.js
@@ -18,10 +18,10 @@ con.connect((err) => {
     console.log('Connection established');
 });
 
-// con.end((err) => {
-//     // The connection is terminated gracefully
-//     // Ensures all previously enqueued queries are still
-//     // before sending a COM_QUIT packet to the MySQL server.
-// });
+con.end((err) => {
+    // The connection is terminated gracefully
+    // Ensures all previously enqueued queries are still
+    // before sending a COM_QUIT packet to the MySQL server.
+});
 
 module.exports.pool = con;
