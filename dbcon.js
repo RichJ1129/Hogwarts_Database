e5c191ed45fff93cdee83accf929502926bb6aac
Richard Joseph <richjoseph1129@gmail.com>
2019-11-25 21:56:57 -0500

    Server error fix

--- dbcon.js
+++ dbcon.js
@@ -4,10 +4,10 @@ var aws = require('aws-sdk');
 
 var con = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_josephri',
-    password        : '1233',
-    database        : 'cs340_josephri'
+    host            : process.env.host,
+    user            : process.env.user,
+    password        : process.env.password,
+    database        : process.env.database
 });
 
 module.exports.pool = con;
