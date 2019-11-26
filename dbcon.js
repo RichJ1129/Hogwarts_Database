17daf8f53bdae0ad449755fc65ccf5953f076360
Richard Joseph <richjoseph1129@gmail.com>
2019-11-26 18:41:40 -0500

    Update Professor

--- dbcon.js
+++ dbcon.js
@@ -3,11 +3,20 @@ var mysql = require('mysql');
 var aws = require('aws-sdk');
 
 var con = mysql.createPool({
-     connectionLimit : 10,
-     host            : process.env.host,
-     user            : process.env.user,
-     password        : process.env.password,
-     database        : process.env.database
- });
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_josephri',
+    password        : '1233',
+    database        : 'cs340_josephri'
+});
+
+
+// var con = mysql.createPool({
+//     connectionLimit : 10,
+//     host            : process.env.host,
+//     user            : process.env.user,
+//     password        : process.env.password,
+//     database        : process.env.database
+// });
 
 module.exports.pool = con;
--- viewProfessor.js
+++ viewProfessor.js
@@ -2,7 +2,7 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    function getStudent(res, mysql, context, done){
+    function getProfessor(res, mysql, context, done){
         var sql = "SELECT professor.professor_id as pID, first_name as fname, last_name as lname, school.name as profSchool, house.name as profHouse, pet.name as profPet, wand.core as profWand FROM professor\n" +
             "LEFT JOIN school ON professor.school = school.school_id\n" +
             "LEFT JOIN house ON professor.house = house.house_id\n" +
@@ -45,7 +45,7 @@ module.exports = function(){
         var context = { title: 'Hogwart\'s HeadMaster Database' };
         context.jsscripts = ["deleteProfessor.js"];
         var mysql = req.app.get('mysql');
-        getStudent(res,mysql, context, done);
+        getProfessor(res,mysql, context, done);
         function done(){
             callbackCount++;
             if(callbackCount >= 1){
@@ -56,7 +56,7 @@ module.exports = function(){
 
     router.get('/:id', function(req, res){
         callbackCount = 0;
-        var context = {};
+        var context = { title: 'Hogwart\'s HeadMaster Database' };
         context.jsscripts = [ "update.js"];
         var mysql = req.app.get('mysql');
         getOnePro(res, mysql, context, req.params.id, complete);
