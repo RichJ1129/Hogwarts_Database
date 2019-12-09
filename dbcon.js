1d5299ce3897aa41074cee6fe4f0887171d85ad4
Richard Joseph <richjoseph1129@gmail.com>
2019-12-09 17:00:52 -0500

    Filter Update

--- dbcon.js
+++ dbcon.js
@@ -2,15 +2,6 @@
 var mysql = require('mysql');
 var aws = require('aws-sdk');
 
-// var con = mysql.createPool({
-//     connectionLimit : 10,
-//     host            : 'classmysql.engr.oregonstate.edu',
-//     user            : 'cs340_josephri',
-//     password        : '1233',
-//     database        : 'cs340_josephri'
-// });
-
-
 var con = mysql.createPool({
     connectionLimit : 10,
     host            : process.env.host,
