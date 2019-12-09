0166709f33fd6fc5e7c68919ea473f28785737a3
EvanSpeciale <speciale@oregonstate.edu>
2019-12-09 11:49:32 -0700

    add pet selector to regStudent

--- regStudent.js
+++ regStudent.js
@@ -2,9 +2,27 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    function getPets(res, mysql, context, done){
+        mysql.pool.query("SELECT pet_id as id, name FROM pet", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.pets = results;
+            done();
+        });
+    }
     router.get('/', function(req, res, next) {
-        res.render('regStudent', { title: 'Hogwart\'s HeadMaster Database' });
-    });
+        var callbackCount = 0;
+        var context = { title: 'Hogwart\'s HeadMaster Database' };
+        var mysql = req.app.get('mysql');
+        getPets(res,mysql,context,done);
+        function done(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('regStudent', context);
+            }
+        }});
 
     router.post('/', function(req, res){
         console.log("Adding Student");
