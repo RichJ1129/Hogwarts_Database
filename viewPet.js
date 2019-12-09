35d181dd8e38b0fb7153de80a4fcb39f980b847a
EvanSpeciale <speciale@oregonstate.edu>
2019-12-09 13:17:43 -0700

    Update viewPet.js

--- viewPet.js
+++ viewPet.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getPet(res, mysql, context, done){
-        var sql = "SELECT pet.pet_id as petID, name as petName, species as petSpecies FROM pet where petID NOT '999'" ;
+        var sql = "SELECT pet.pet_id as petID, name as petName, species as petSpecies FROM pet where petID NOT 999" ;
         mysql.pool.query(sql,
             function(error, results, fields){
                 if(error){
