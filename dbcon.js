eea1791085e29aac089e85161a8d090d17410d1a
EvanSpeciale <speciale@oregonstate.edu>
2019-12-08 20:30:04 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -4,6 +4,9 @@ var aws = require('aws-sdk');
 
 var con = mysql.createPool({
     connectionLimit : 10,
+    connectionTimeout: 60*60*1000,
+    acquireTimeout  : 60*60*1000,
+    timeout         : 60*60*1000,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_josephri',
     password        : '1233',
